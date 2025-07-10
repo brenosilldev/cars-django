fdc998a4a05adee13516b2fad141d616b0979fd1
brenosilldev <brenosill@hotmail.com>
2025-07-09 23:29:03 -0300

    Subida de arquivos

--- carros/admin.py
+++ carros/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from carros.models import Carro as Card
+from carros.models import Marca
 
 # Register your models here.
+
+class Carro(admin.ModelAdmin):
+    list_display = ( 'modelo', 'ano', 'cor','anofabricacao','anofabricacao','idmarca')
+    search_fields = ('modelo',)
+
+
+class CarroMarca(admin.ModelAdmin):
+    list_display = ('marca',)
+    search_fields = ('marca',)
+
+
+
+# Registro de tabelas para usar
+admin.site.register(Card, Carro)
+admin.site.register(Marca, CarroMarca)
--- carros/models.py
+++ carros/models.py
@@ -2,10 +2,32 @@ from django.db import models
 
 # Create your models here.
 
+
+class Marca(models.Model):
+    idmarca = models.AutoField(primary_key=True)
+    marca = models.CharField(max_length=100)
+
+
+    # def __str__(self): retorna o valor que vai ser mostrado na tela
+    def __str__(self):
+        return self.marca
+
+
+
+
 class Carro(models.Model):
     idcarro = models.AutoField(primary_key=True)
-    marca = models.CharField(max_length=100)
-    cor = models.CharField(max_length=100)
     modelo = models.CharField(max_length=100)
+    idmarca = models.ForeignKey(Marca, on_delete=models.PROTECT,related_name='carros_marca',null=True, blank=True)
+    cor = models.CharField(max_length=100)
     ano = models.IntegerField(blank=True, null=True)
     anofabricacao = models.IntegerField(blank=True, null=True)
+    placa = models.CharField(max_length=30, null=True, blank=True)
+    foto = models.ImageField(upload_to='carros/fotos/', null=True, blank=True)
+
+
+    def __str__(self):
+        
+        return self.modelo
+    
+
